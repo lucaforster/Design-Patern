a721275d9415c7e0c88d0bb996afd0d1ba9923ee
lucaforster <luca.forster1@gmail.com>
2022-11-04 09:25:19 +0100

    first commit

---
+++ Design-Pattern/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Design_Pattern;
+
+SingeltonPattern Peter = SingeltonPattern.getInstance();
+
+Peter.setName("Peter");
+
+Console.WriteLine(Peter.Bestellung());
+
+SingeltonPattern Manfred = SingeltonPattern.getInstance();
+
+Manfred.setName("Manfred");
+
+Console.WriteLine(Manfred.Bestellung());
+Console.WriteLine(Peter.Bezahlung());
---
+++ Design-Pattern/SingeltonPattern.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Pattern
+{
+    public class SingeltonPattern
+    {
+        public string name;
+
+        private static SingeltonPattern Waitor;
+
+        private SingeltonPattern()
+        {
+        }
+        
+        public static SingeltonPattern getInstance()
+        {
+            if (Waitor == null)
+            {
+                Waitor = new SingeltonPattern();
+            }
+            return Waitor;
+        }
+
+
+        public void setName(string Name)
+        {
+            name = Name;
+        }
+
+        public string Bestellung()
+        {
+            return name + "Was möchten sie bestellen?";
+        }
+
+        public string Bezahlung()
+        {
+            return name + "Möchten sie schon bezahlen";
+        }
+    }
+}
